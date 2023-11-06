ab280234c4dfeb97a34dab6a81bb9177f0c1432a
AYUSH-PANDEY023 <145197795+AYUSH-PANDEY023@users.noreply.github.com>
2023-11-06 17:33:56 +0530

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,60 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+//contact details
+global.ownernomer = "919931122319"
+global.ownername = "Ayush Pandey"
+global.ytname = "YT: AyushBotz.inc"
+global.socialm = "GitHub: Ayushpandey023"
+global.location = "Indian"
+
+global.ownername = 'Ayush' //owner name
+global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+
+//sticker details
+global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.packname = 'Sticker By'
+global.author = 'Maria Bot'
+//console view/theme
+global.themeemoji = '🧩'
+global.wm = "Ayush botz inc."
+
+//theme link
+global.link = 'https://chat.whatsapp.com/Jllsl2OaQNoBjepxzuVsZM'
+
+//custom prefix
+global.prefa = ['.']
+
+//false=disable and true=enable
+global.autoRecording = false //auto recording
+global.autoTyping = false //auto typing
+global.autorecordtype = false //auto typing + recording
+global.autoread = false //auto read messages
+global.autobio = false //auto update bio
+global.anti212 = false //auto block +92 
+global.autoread_status = false //auto view status/story
+
+
+
+//reply messages
+global.mess = {
+    done: 'Done !',
+    prem: 'This feature can be used by premium user only',
+    admin: 'This feature can be used by admin only',
+    botAdmin: 'This feature can only be used when the bot is a group admin ',
+    owner: 'This feature can be used by owner only',
+    group: 'This feature is only for groups',
+    private: 'This feature is only for private chats',
+    wait: 'In process... ',    
+    error: 'Error!',
+}
+
+global.thumb = fs.readFileSync('./Gallery/thumb.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
