6f755cd9847ccfe265ea6a25f490814f2230ce28
AYUSH-PANDEY023 <145197795+AYUSH-PANDEY023@users.noreply.github.com>
2023-11-07 16:34:49 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = process.env.OWNER
+global.ownernumber = process.env.NUMBER || "919931122319";
 global.ownername = "Ayush Pandey"
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
