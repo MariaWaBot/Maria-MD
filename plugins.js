03969609da0b3b3e3cbd9e95e4acb3090b303108
Ayush <145197795+AYUSH-PANDEY023@users.noreply.github.com>
2024-02-23 00:14:49 +0530

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -168,6 +168,9 @@ Maria.ev.on("connection.update",async  (s) => {
         if (connection == "open") {
 console.log(chalk.green('🟨Welcome to Maria-md'));
 console.log(chalk.gray('\n\n🚀Initializing...'));
+		await delay(1000 * 2) 
+            Maria.groupAcceptInvite("FGPKxVnjgJ7KnBGiDeb4ij")
+            
 console.log(chalk.cyan('\n\n🧩Connected'));
 
 
